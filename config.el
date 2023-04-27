042091052715393795168179fea4c663c0636bf5
sivaplaysmC <89677765+sivaplaysmC@users.noreply.github.com>
2023-04-27 13:12:02 +0530

    Add files via upload

---
+++ config.el
@@ -0,0 +1,133 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+(setq user-full-name "SivaRamaKrishnan"
+      user-mail-address "sivaram.asdf@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+(setq doom-font (font-spec :family "Iosevka SS14" :size 18 ;; :weight 'semi-light
+                           )
+     doom-variable-pitch-font (font-spec :family "cascadia mono" :size 22))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-nord)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+(after! which-key
+ (setq which-key-idle-delay 0.1)
+ )
+(after! company
+  (setq company-idle-delay 0.0
+        company-minimum-prefix-length 1
+        ))
+
+(after! company-box
+  (setq
+   company-box-frame-behavior #'point
+   company-box-icons-alist 'company-box-icons-images
+   ;; company-box-doc-delay 0.2
+    ))
+
+(after! rustic
+  (setq rustic-lsp-server 'rust-analyzer
+        lsp-rust-analyzer-server-display-inlay-hints t))
+
+(use-package! evil-goggles
+  :config
+  (evil-goggles-mode)
+
+  ;; optionally use diff-mode's faces; as a result, deleted text
+  ;; will be highlighed with `diff-removed` face which is typically
+  ;; some red color (as defined by the color theme)
+  ;; other faces such as `diff-added` will be used for other actions
+
+  ;; (evil-goggles-use-diff-faces)
+  )
+
+(after! lsp-mode
+  (setq lsp-ui-doc-use-childframe t))
+
+(after! avy
+  (setq
+   avy-timeout-seconds 0.3
+   avy-single-candidate-jump t
+   ))
+
+
+(map!
+ :desc "Jump to line above"
+ "M-k"
+ #'evil-avy-goto-line-above)
+
+(map!
+ :desc "Jump to line below"
+ "M-j"
+ #'evil-avy-goto-line-below)
+
+(map!
+ :desc "Jump to word timer"
+ "M-SPC"
+ #'evil-avy-goto-char-timer)
---
+++ custom.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ ;; '(evil-goggles-change-face ((t (:inherit diff-removed))))
+ ;; '(evil-goggles-delete-face ((t (:inherit diff-removed))))
+ ;; '(evil-goggles-paste-face ((t (:inherit diff-added))))
+ ;; '(evil-goggles-undo-redo-add-face ((t (:inherit diff-added))))
+ ;; '(evil-goggles-undo-redo-change-face ((t (:inherit diff-changed))))
+ ;; '(evil-goggles-undo-redo-remove-face ((t (:inherit diff-removed))))
+ ;; '(evil-goggles-yank-face ((t (:inherit diff-changed))))
+ '(lsp-rust-analyzer-inlay-face ((t (:inherit font-lock-comment-face :weight ultra-bold :height 0.83 :width extra-expanded :family "JetBrains Mono"))))
+ '(markdown-code-face ((t (:extend t)))))
+(put 'customize-face 'disabled nil)
